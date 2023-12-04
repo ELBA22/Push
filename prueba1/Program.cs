22c601f5713e33fe27ead1d21df370abec3a53c9
ELBA22 <137318270+ELBA22@users.noreply.github.com>
2023-12-04 17:16:59 -0500

    feat: :sparkles:

--- prueba1/Program.cs
+++ prueba1/Program.cs
@@ -1,7 +1,81 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("FIBONACCI");
+        Console.WriteLine("1. ");
+        Console.WriteLine("2.  ");
+        Console.WriteLine("3.  ");
+        Console.WriteLine("4. SALIR");
+        Console.WriteLine("Ingrese una opcion: ");
+        var opcion = Convert.ToInt32(Console.ReadLine());
+        bool controlador = true;
+        while (controlador)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    Console.Clear();
+                    numFibo();
+                    break;
+
+
+                case 2:
+                    Console.Clear();
+                    sinoNumFib();
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    primerNum();
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    controlador = false;
+                    break;
+
+                default:
+                break;
+            }
+        }
+        
+    }
+
+    public static void  numFibo()
+        {
+            Console.WriteLine("Escriba un numero entero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("Ingrese un numero positivo");
+    
+            }
+            else 
+            {
+                int a =0,b=1, resultado= a+b;
+                a = b;
+                b= resultado;
+                resultado = a+b;
+                Console.WriteLine($"f{n}");
+            }
+        }
+
+    public static void sinoNumFib()
+    {
+        Console.WriteLine("Ingrese un numero entero: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+    }
+
+
+    public static void primerNum()
+    {
+        Console.WriteLine("Ingrese un numero entero: ");
     }
 }
+
+
--- prueba1/obj/Debug/net8.0/prueba1.AssemblyInfo.cs
+++ prueba1/obj/Debug/net8.0/prueba1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6072b9c71d197c70284fe63eeced1d2f31f0dde7")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
